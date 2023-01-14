992f9531c22d031b08801768d6df49514f7b90ab
Ragnar Olafsson <ragnar.orn@gmail.com>
2023-01-14 11:04:52 +0000

    Finish the product model

--- django_orm/inventory/models.py
+++ django_orm/inventory/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Brand(models.Model):
     brand_id = models.BigAutoField(primary_key=True)
-    name = models.CharField(max_length=50)
+    name = models.CharField("Brand Name", max_length=50)
 
     def __str__(self) -> str:
         return f"Brand name: {self.name}"
@@ -16,22 +16,12 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    the_name = models.CharField(
-        "Product Name",
-        max_length=100,
-        default="no-name",
-        help_text="This is the help text",
-    )
-    age = models.IntegerField()
-    is_active = models.BooleanField(default=True)
-    # category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    category = models.ManyToManyField(Category)
-
-    class Meta:
-        ordering = ["age"]
-
-    def __str__(self):
-        return f"Product name: {self.name}"
+    name = models.CharField(max_length=10)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    url = models.SlugField()
+    is_active = models.BooleanField()
 
 class Stock(models.Model):
     units = models.BigIntegerField()
