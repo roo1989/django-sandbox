8ffd32bdb1d9ff34fa80d8dda6f1e57f0d208003
Ragnar Olafsson <ragnar.orn@gmail.com>
2023-01-17 13:34:16 +0000

    Create a viewset for the company api

--- django-pytest/api/companies/views.py
+++ django-pytest/api/companies/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from companies.models import Company
+from companies.serializers import CompanySerializer
+
+
+class CompanyViewSet(ModelViewSet):
+    serializer_class = CompanySerializer
+    queryset = Company.objects.all().order_by("-last_updated")
+    pagination_class = PageNumberPagination
