0729857c4493a39444489ba994ad95fe6d4ce734
Ragnar Olafsson <ragnar.orn@gmail.com>
2023-01-17 13:33:41 +0000

    Add serializer to companies

---
+++ django-pytest/api/companies/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from companies.models import Company
+
+class CompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Company
+        fields = ("id", "name", "status", "application_link", "last_updated", "notes")
