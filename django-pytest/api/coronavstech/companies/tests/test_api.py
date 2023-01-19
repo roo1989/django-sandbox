ee552cf699ec8b404101e31464717fdc7904ef96
Ragnar Olafsson <ragnar.orn@gmail.com>
2023-01-19 16:02:28 +0000

    Refactor

--- django-pytest/api/coronavstech/companies/tests/test_api.py
+++ django-pytest/api/coronavstech/companies/tests/test_api.py
@@ -7,17 +7,16 @@ from django.urls import reverse
 
 from companies.models import Company
 
-
 @pytest.mark.django_db
-class TestGetCompanies(TestCase):
+class BasicCompanyAPITestCase(TestCase):
     def setUp(self) -> None:
         self.client = Client()
         self.companies_url = reverse("companies-list")
 
-    
     def tearDown(self) -> None:
         pass
 
+class TestGetCompanies(BasicCompanyAPITestCase):
     def test_zero_companies_should_return_an_empty_list(self) -> None:
         response = self.client.get(self.companies_url)
         self.assertEqual(response.status_code, 200)
@@ -34,3 +33,22 @@ class TestGetCompanies(TestCase):
         self.assertEqual(response_content.get("notes"), "")
 
         test_company.delete()
+
+class TestPostCompanies(BasicCompanyAPITestCase):
+    def test_create_company_without_arguments_should_fail(self) -> None:
+        response = self.client.post(self.companies_url)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(
+            json.loads(response.content), {"name": ["This field is required."]}
+        )
+
+    def test_create_existing_company_should_fail(self) -> None:
+        Company.objects.create(name="Amazon")
+        post_response = self.client.post(self.companies_url, data={"name": "Amazon"})
+        self.assertEqual(post_response.status_code, 400)
+        self.assertEqual(
+            json.loads(post_response.content), {"name": ["company with this name already exists."]}
+        )
+
+    def test_create_company_only_with_name_all_fields_should_default_to_model_defaults(self) -> None:
+        pass
