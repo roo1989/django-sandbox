98d8dea8f44d8703169c8a887d94740169841645
Ragnar Olafsson <ragnar.orn@gmail.com>
2023-01-20 15:29:58 +0000

    Add email functionality

--- django-pytest/api/coronavstech/companies/views.py
+++ django-pytest/api/coronavstech/companies/views.py
@@ -1,11 +1,20 @@
+from django.core.mail import send_mail
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import api_view
+from rest_framework.request import Request
+from rest_framework.response import Response
 
-from .models import Company
-from .serializers import CompanySerializer
+from companies.models import Company
+from companies.serializers import CompanySerializer
 
 
 class CompanyViewSet(ModelViewSet):
     serializer_class = CompanySerializer
     queryset = Company.objects.all().order_by("-last_update")
     pagination_class = PageNumberPagination
+
+@api_view(http_method_names=["POST"])
+def send_company_email(request: Request) -> Response:
+    send_mail(subject="My cool subject", message="My cool message", from_email="ragnar.orn@gmail.com", recipient_list=["ragnar.orn@gmail.com"])
+    return Response({"status": "Success", "info": "Email sent successfully", "status": 200})
